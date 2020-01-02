618ec5b2bc0e7ccc59eb47a4f4f068e6f6b6d4e2
shirai1 <shirai1@macnoMacBook-Pro.local>
2020-01-02 20:34:36 +0900

    [add] pure flask

---
+++ flask_test3/pure_flask/app.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+from flask import Flask, jsonify, request
+import os
+app = Flask(__name__)
+flask_conf_file = os.path.join(os.getcwd(), 'conf', 'flask_conf.cfg')
+mail_file = os.path.join(os.getcwd(), '..', 'data', 'personal_info.csv')
+app.config.from_pyfile(flask_conf_file)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    result_dict = {}
+    mail_address_list = []
+    target_prefecture = request.args.get('pref')
+    with open(mail_file, 'r') as inf:
+        for low_num, line in enumerate(inf):
+            if low_num == 0:  # header
+                continue
+            line = line.rstrip()
+            vals = line.split(',')
+            mail_add, sex, age, name, prefecture = vals
+            if prefecture == target_prefecture:
+                mail_address_list.append(mail_add)
+
+    result_dict['mail_address_list'] = mail_address_list
+    return jsonify(result_dict)
+
+
+if __name__ == '__main__':
+    app.run()
